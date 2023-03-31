96ffd7b7a42b9beec1d5d88efa5121d8ab48f970
Markuss BalodiŠ <markussdove@gmail.com>
2023-03-31 11:47:44 +0300

    HTML uz PHP

---
+++ aktualitate.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aktualitāte</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+</head>
+<body>
+    <div class="page-container">
+<header>
+    <div class="logo">
+    <a href="index.html"> IT ir spēks! </a>
+    </div>
+
+<nav class="navbar">
+    <a href="index.html" onload="enableOverflow()">Sākums</a>
+    <a href="aktualitates.html">Aktualitātes</a>
+    <a href="vakances.html">Vakances</a>
+    <a href="kontakti.html">Kontakti</a>
+    <a id="login-btn" onclick="openForm()"><i class="fas fa-sign-in"></i></a>
+    
+    <div class="login-popup">
+        <div class="close" onclick="closeForm()">×</div>
+        <h2>Pieslēgties</h2>
+        <form>
+          <input type="text" placeholder="Lietotājvārds" required>
+          <input type="password" placeholder="Parole" required>
+          <button type="submit">Autorizēties</button>
+        </form>
+      </div>
+      <div class="page-container">
+</nav>
+<div id="menu-btn" class="fas fa-bars"></div>
+</header>
+
+<div class="box-container">
+    <div class="box">
+<h2>Noteiktā aktualitāte</h2>
+<hr>
+<p class="cont">Noteiktās aktualitātes informācija šeit būs aprakstīta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aliquid ab eligendi delectus tempore earum autem qui voluptates dolores minima. Quas, illum! Maxime rerum facilis, delectus molestias asperiores eum consequatur! Et dolores tempora, odit alias dolor accusantium sed maxime sit consequatur non odio assumenda qui quas omnis aliquid! Cumque dolorem quaerat libero sequi exercitationem ipsum quae, incidunt quasi, rerum hic voluptas aperiam earum quas. Vitae alias amet, vero, minima nemo molestias unde quasi maiores distinctio optio ratione? Est quos exercitationem dolores ducimus architecto illum deserunt totam iure voluptas aperiam. Mollitia reprehenderit possimus hic iure laboriosam at, omnis fugiat! Illo placeat temporibus consequatur nulla itaque eligendi quo nihil asperiores nemo fugiat error id quisquam unde similique perferendis a, culpa doloremque omnis ex ut? Beatae blanditiis doloremque sequi rem ut facilis consequuntur repellendus. Error iusto enim sunt consectetur iure est eum quasi tempore dicta saepe expedita tenetur, nisi aperiam architecto, recusandae suscipit dolores veniam autem nulla assumenda ipsum in sint et minima! Commodi odit sunt sapiente molestias. Voluptatum neque rem temporibus officiis doloremque dicta nam id, repellat tempora molestias labore placeat autem iure quod exercitationem optio. Aspernatur aliquid impedit accusamus at ad cum soluta, quos possimus reprehenderit tempora adipisci ab nesciunt aperiam atque magnam veritatis perferendis optio quaerat animi? Repellat officia ipsam pariatur adipisci. Veritatis enim optio corporis in amet, facere reiciendis molestias tenetur ipsam maiores! Dolorum, perspiciatis labore neque ea autem ab aut non voluptatum tenetur debitis saepe, maiores accusantium delectus explicabo, quae soluta voluptates iure totam animi perferendis vel eum libero distinctio enim. Similique a repellat nostrum animi accusamus vel non at ducimus beatae. Reiciendis consequuntur voluptatem ex, excepturi repudiandae ab cumque soluta minus magni, modi minima! Non accusamus doloremque aut eveniet nostrum reprehenderit cum id quia sit at repudiandae assumenda est, quas recusandae accusantium neque itaque nesciunt tempora consequuntur temporibus reiciendis! Sint, nulla minus! Debitis neque obcaecati impedit exercitationem provident quia laborum molestiae nobis laudantium! Quia aliquam libero repellat provident animi accusamus non amet voluptatibus aperiam officia aspernatur similique doloribus odit assumenda necessitatibus quasi, perferendis saepe ipsam! Quo velit non dolore atque repellat culpa sint ratione, error cum, beatae voluptatibus ad nesciunt? Fugit, debitis. Iusto ducimus iure qui laboriosam dolorum placeat dolore aperiam, illum ipsam eligendi praesentium? Consequatur est cupiditate iure nulla labore, optio possimus a qui fugiat architecto veniam tenetur quasi nemo earum suscipit ducimus eveniet nesciunt illum, animi reprehenderit! Consequatur ipsum dolorem blanditiis minima? Accusantium, ut fugiat. Hic magnam exercitationem, atque eius deleniti cum quibusdam totam quaerat voluptates possimus ab suscipit nesciunt molestiae quia distinctio eum explicabo dicta culpa numquam dignissimos assumenda nostrum ipsum reprehenderit? Nam ex consequuntur ut quam atque! Nemo excepturi natus doloribus minima magnam commodi quia distinctio alias ab nam ea hic illo, tempora, eius eligendi eaque ipsa nulla totam accusantium blanditiis repellendus. Quam nesciunt beatae in a illum voluptate magni asperiores. Voluptates unde nihil eius. Architecto possimus blanditiis excepturi accusamus reprehenderit numquam quo similique dolorum recusandae omnis placeat at repellendus eligendi assumenda porro, quisquam iusto. Corrupti enim esse repudiandae optio culpa officia nemo.</p>
+<div class="authDate">
+<p>Datums: 11.09.2001</p>
+<p>Autors: Marguze</p>
+</div>
+    </div>
+</div>
+<footer>
+<h1>IT ir Spēks &copy; 2023</h1>
+<hr>
+<p class="STD"><b>S</b>mart <b>T</b>ech <b>D</b>evelopment<p>
+</footer>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+<script src="script.js"></script>
+</body>
+</html>
